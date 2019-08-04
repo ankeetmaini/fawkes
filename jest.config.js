8fe5d72d062132e2a68a1925b7c47e23675a5a4a
Ankeet Maini <ankeet.maini@gmail.com>
2019-08-04 19:27:00 +0530

    ci(codecov): add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/packages/**/*{js,ts,jsx,tsx}',
+    '!**/node_modules/**',
+  ],
+  coverageReporters: ['lcov', 'text-summary'],
 };
